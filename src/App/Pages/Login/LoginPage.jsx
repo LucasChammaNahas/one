c4dd46ddf2c77f18d631fb54bb8574e3334952a5
Lucas Chamma Nahas <lucasnahas@gmail.com>
2022-08-29 23:25:13 +0100

    Adds logo and style to login page

--- src/App/Pages/Login/LoginPage.jsx
+++ src/App/Pages/Login/LoginPage.jsx
@@ -2,7 +2,8 @@ import { createContext, useState } from "react";
 import LoginButton from "./Components/LoginButton";
 import LoginEmailInput from "./Components/LoginEmailInput";
 import LoginPasswordInput from "./Components/LoginPasswordInput";
-import { submitLogin } from "./Functions/loginSubmit"
+import { submitLogin } from "./Functions/loginSubmit";
+import pandaImg from "App/Assets/Images/panda1.png";
 
 export const LoginContext = createContext(null);
 
@@ -16,15 +17,17 @@ export default function LoginPage() {
     <LoginContext.Provider value={{ loginState, setLoginState }}>
       <div className="h-screen flex justify-center items-center">
         <div className="card w-96 bg-base-300 shadow-xl p-16">
+          <div className="flex justify-center pb-10">
+            <img className="h-44" src={pandaImg} alt="logo" />
+          </div>
           <h2 className="text-secondary text-4xl">Login</h2>
-          <form onSubmit={handleFormSubmit}>
-            <p>Please, log in to continue</p>
+          <p className="pt-1">Please, log in to continue</p>
+          <form onSubmit={handleFormSubmit} className="flex flex-col space-y-4 pt-6">
             <LoginEmailInput />
             <LoginPasswordInput />
             <LoginButton />
-            <p>mail: {loginState.email}</p>
-            <p>password: {loginState.pwd}</p>
           </form>
+          <p className="pt-4">Don't have an account? <span className="text-secondary font-bold">sign up</span></p>
         </div>
       </div>
     </LoginContext.Provider>
