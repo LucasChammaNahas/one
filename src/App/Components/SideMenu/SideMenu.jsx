79d5886e55a8a84d450c0b0a8ec7fdcbd246edb2
Lucas Chamma Nahas <lucasnahas@gmail.com>
2022-11-29 15:45:14 +0000

    save

--- src/App/Components/SideMenu/SideMenu.jsx
+++ src/App/Components/SideMenu/SideMenu.jsx
@@ -1,26 +1,22 @@
 import { Link, useLocation } from "react-router-dom";
-import { useAppContext } from 'App/StateManagement/useContexts';
 
 export default function SideMenu() {
-   const { appState } = useAppContext()
-   // const { currentPage } = appState;
-   const navState = useLocation().state || {};
-   const { currentPage } = navState;
+   const currentPage = useLocation()?.state?.currentPage;
    console.log('--> ', useLocation())
 
    return (
       <div className="border-4 border-indigo-500">
          <h2>Navigate</h2>
-         <Link to="menus" state={{...navState, currentPage: 'menus'}}>
+         <Link to="menus">
             <p className={`${currentPage === 'menus' ? 'text-green-300' : ''}`}>Menus</p>
          </Link>
-         <Link to="meals" state={{...navState, currentPage: 'meals'}}>
+         <Link to="meals">
             <p className={`${currentPage === 'meals' ? 'text-green-300' : ''}`}>Meals</p>
          </Link>
-         <Link to="dishes" state={{...navState, currentPage: 'dishes'}}>
+         <Link to="dishes">
             <p className={`${currentPage === 'dishes' ? 'text-green-300' : ''}`}>Dishes</p>
          </Link>
-         <Link to="ingredients" state={{...navState, currentPage: 'ingredients'}}>
+         <Link to="ingredients">
             <p className={`${currentPage === 'ingredients' ? 'text-green-300' : ''}`}>Ingredients</p>
          </Link>
       </div>
